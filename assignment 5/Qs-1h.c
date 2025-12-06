2d3ce6cd982e75667f14ed972b3d73464de3b72c
harshit0789 <harshitmansar@gmail.com>
2025-12-06 17:44:01 +0530

    Update Qs-1h.c

--- assignment 5/Qs-1h.c
+++ assignment 5/Qs-1h.c
@@ -1,23 +1,30 @@
-//print_list_only.c
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-struct Node { int data; struct Node* next; };
+class Node {
+public:
+    int data;
+    Node* next;
 
-struct Node* createNode(int d){ struct Node* n = malloc(sizeof(struct Node)); if(!n){ perror("malloc"); exit(EXIT_FAILURE);} n->data=d; n->next=NULL; return n; }
+    Node(int d){ data = d; next = NULL; }
+};
 
-void push_front(struct Node** headRef, int data){ struct Node* n = createNode(data); n->next = *headRef; *headRef = n; }
+int main() {
 
-void printList(struct Node* head){ while(head){ printf("%d -> ", head->data); head = head->next; } printf("NULL\n"); }
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
 
-void freeList(struct Node** headRef){ struct Node* cur = *headRef; while(cur){ struct Node* nxt = cur->next; free(cur); cur = nxt; } *headRef = NULL; }
+    cout << "Linked List: ";
 
-int main(){
-    struct Node* head = NULL;
-    push_front(&head, 30);
-    push_front(&head, 20);
-    push_front(&head, 10);
-    printf("List content:\n"); printList(head);
-    freeList(&head);
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+
     return 0;
 }
+
+
