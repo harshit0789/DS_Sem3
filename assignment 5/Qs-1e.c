e7f3311fc1f46b3ba07271a82e5f163649ae0677
harshit0789 <harshitmansar@gmail.com>
2025-12-06 17:42:09 +0530

    Convert C linked list implementation to C++

--- assignment 5/Qs-1e.c
+++ assignment 5/Qs-1e.c
@@ -1,47 +1,50 @@
-// delete_end.c
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-struct Node { int data; struct Node* next; };
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int d){ data = d; next = NULL; }
+};
 
-struct Node* createNode(int d){
-    struct Node* n = malloc(sizeof(struct Node));
-    if (!n) { perror("malloc"); exit(EXIT_FAILURE); }
-    n->data = d; n->next = NULL; return n;
+int main() {
+
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    if (head == NULL) {
+        cout << "List empty!";
+        return 0;
+    }
+
+    if (head->next == NULL) {
+        cout << "Deleted: " << head->data;
+        delete head;
+        head = NULL;
+        return 0;
+    }
+
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    cout << "Deleted: " << temp->data << endl;
+    prev->next = NULL;
+    delete temp;
+
+    cout << "Updated List: ";
+    Node* t = head;
+    while (t) {
+        cout << t->data << " -> ";
+        t = t->next;
+    }
+    cout << "NULL";
 }
 
-void push_front(struct Node** headRef, int data){
-    struct Node* n = createNode(data); n->next = *headRef; *headRef = n;
-}
 
-void deleteEnd(struct Node** headRef){
-    if (*headRef == NULL) return;
-    if ((*headRef)->next == NULL){ free(*headRef); *headRef = NULL; return; }
-    struct Node* cur = *headRef;
-    while(cur->next->next) cur = cur->next;
-    free(cur->next);
-    cur->next = NULL;
-}
-
-void printList(struct Node* head){
-    while(head){ printf("%d -> ", head->data); head = head->next; }
-    printf("NULL\n");
-}
-
-void freeList(struct Node** headRef){
-    struct Node* cur = *headRef;
-    while(cur){ struct Node* nxt = cur->next; free(cur); cur = nxt; }
-    *headRef = NULL;
-}
-
-int main(){
-    struct Node* head = NULL;
-    push_front(&head, 30);
-    push_front(&head, 20);
-    push_front(&head, 10); // 10->20->30
-    printf("Before deleteEnd:\n"); printList(head);
-    deleteEnd(&head);
-    printf("After deleteEnd:\n"); printList(head);
-    freeList(&head);
-    return 0;
-}
