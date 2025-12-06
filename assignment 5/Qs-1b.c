2bda540b271727566d3e4980a01c65c45e735a2b
harshit0789 <harshitmansar@gmail.com>
2025-12-06 17:39:45 +0530

    Update Qs-1b.c

--- assignment 5/Qs-1b.c
+++ assignment 5/Qs-1b.c
@@ -1,41 +1,33 @@
-// push_end.c
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-struct Node { int data; struct Node* next; };
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int d) { data = d; next = NULL; }
+};
 
-struct Node* createNode(int d){
-    struct Node* n = malloc(sizeof(struct Node));
-    if (!n) { perror("malloc"); exit(EXIT_FAILURE); }
-    n->data = d; n->next = NULL; return n;
+int main() {
+
+    Node* head = NULL;
+
+    int val;
+    cout << "Enter value to insert at end: ";
+    cin >> val;
+
+    Node* newNode = new Node(val);
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        Node* temp = head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+
+    cout << "Inserted at end. Final list: " << head->data << " -> NULL";
 }
 
-void push_end(struct Node** headRef, int data){
-    struct Node* n = createNode(data);
-    if (*headRef == NULL) { *headRef = n; return; }
-    struct Node* cur = *headRef;
-    while(cur->next) cur = cur->next;
-    cur->next = n;
-}
 
-void printList(struct Node* head){
-    while(head){ printf("%d -> ", head->data); head = head->next; }
-    printf("NULL\n");
-}
-
-void freeList(struct Node** headRef){
-    struct Node* cur = *headRef;
-    while(cur){ struct Node* nxt = cur->next; free(cur); cur = nxt; }
-    *headRef = NULL;
-}
-
-int main(){
-    struct Node* head = NULL;
-    push_end(&head, 30);
-    push_end(&head, 20);
-    push_end(&head, 10);
-    printf("After push_end calls:\n");
-    printList(head);
-    freeList(&head);
-    return 0;
-}
